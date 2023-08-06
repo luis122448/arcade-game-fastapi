786b3db87404972d95b8ca48d9feed8f3a43abcb
luis122448 <luis122448@gmail.com>
2023-08-05 19:50:34 -0500

    Add Port configuration

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from middleware.error_handler import ErrorHandler
 from routers.score_routers import score_router
 import uvicorn
 from config.postgres import base, engine
+import os
 
 app = FastAPI()
 app.title = "FastAPI with SQLAlchemy"
 app.version = "0.0.1"
 app.description = "This is a very fancy project, with auto docs for the API and everything"
+app.docs_url = "/docs"
 
 app.add_middleware(ErrorHandler)
 app.include_router(score_router)
@@ -20,6 +22,6 @@ async def Home():
 if __name__ == '__main__':
     uvicorn.run("main:app",
                 host="0.0.0.0",
-                port=8000,
+                port=int(os.environ.get("PORT", 8000)),
                 reload=True,
                 )

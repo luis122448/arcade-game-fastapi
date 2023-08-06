8c693a944c7c38f8960f4c04dd8aec786d03406f
luis122448 <luis122448@gmail.com>
2023-08-05 20:22:53 -0500

    Environment variable

--- config/postgres.py
+++ config/postgres.py
@@ -1,8 +1,20 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "postgresql://postgres:gVLAQSbfCuoKVQl0vvSL@containers-us-west-131.railway.app:5689/railway"
+# Cargar las variables de entorno
+load_dotenv()
+
+DB_USERNAME = os.getenv("DB_USERNAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOSTNAME = os.getenv("DB_HOSTNAME")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+# Crear la URL de conexión a la base de datos
+DATABASE_URL = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOSTNAME}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(DATABASE_URL ,echo=False)
 session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
